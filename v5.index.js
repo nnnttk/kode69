241293f5561b59f469f35da34ef049ce3d98916a
nnnttk <126535843+nnnttk@users.noreply.github.com>
2023-06-04 01:09:19 +0700

    Update and rename v4.index.js to v5.index.js

--- v5.index.js
+++ v5.index.js
@@ -15,12 +15,15 @@ return string;}}
   
   if(/^\?fbclid=/.test(location.search))
      location.replace(location.href.replace(/\?fbclid.+#/, "#"));
-  
-function myFunction() {
-    var awal = Math.floor(Math.random() * 10);
-    var sekend = Math.floor(Math.random() * 10);
-    let akhiran = awal + sekend;
-    
+
+var awal = Math.floor(Math.random() * 10);
+var sekend = Math.floor(Math.random() * 10);
+let akhiran = awal + sekend;
+$(document).ready(function(){
+    $('input[id^="konfirmasi"]').each(function(){  $(this).attr("placeholder", awal+'+'+sekend+'=');  });
+});    
+
+function myFunction() {   
   var uriD = window.location.href;
   var uriS = uriD.replace("?m=1","");
   var uriN = uriS.replace(/http.+html#/,"");
@@ -30,7 +33,6 @@ function myFunction() {
   var hiburanBnya = '" width="100%" height="700" scrolling="no" frameborder="0" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" ></iframe>';
   var fblik = '<a href="https://t.me/s/'+userNameTelegram+'"><button style="width:100%;text-align:center;height:50px">Gabung dan dapatkan notif update via telegram</button></a><br />';
   var konf = document.getElementById("konfirmasi").value;
-  var konfigka = document.getElementById("konfirmasi"); konfigka.setAttribute('placeholder', awal+'+'+sekend+'=');
   var Lasli = '<div style="width:100%;overflow:scroll">Jika video lama dimuat, gunakan tautan ini: <a href="'+UriZ+'" rel="nofollow">'+UriZ+'</a></div>';
       if (konf == akhiran)  {   
        document.write(hiburanDnya+UriZ+hiburanBnya+fblik+Lasli);
